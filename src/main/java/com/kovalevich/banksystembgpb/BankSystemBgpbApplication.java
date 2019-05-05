2eacd4ecfab6898b6ee95ccd1d5a10320049f9fa
Gleb Remniov <remniov.work@gmail.com>
2019-05-05 16:58:32 +0300

    Initial commit

---
+++ src/main/java/com/kovalevich/banksystembgpb/BankSystemBgpbApplication.java
@@ -0,0 +1,13 @@
+package com.kovalevich.banksystembgpb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSystemBgpbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankSystemBgpbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kovalevich/banksystembgpb/BankSystemBgpbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kovalevich.banksystembgpb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BankSystemBgpbApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
