566ae8e933b67c4f063e1b5868f06108a446d1bc
christinakovalevich <christina.kovalevich@gmail.com>
2019-05-09 12:01:16 +0300

    Adding logger to the main class

--- src/main/java/com/kovalevich/banksystembgpb/BankSystemBgpbApplication.java
+++ src/main/java/com/kovalevich/banksystembgpb/BankSystemBgpbApplication.java
@@ -1,13 +1,20 @@
 package com.kovalevich.banksystembgpb;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BankSystemBgpbApplication {
 
+    private final static Logger log = LoggerFactory.getLogger(BankSystemBgpbApplication.class);
+
+    private final static String url = "http://localhost:8080/bank-system";
+
     public static void main(String[] args) {
         SpringApplication.run(BankSystemBgpbApplication.class, args);
+        log.debug(url);
     }
 
 }
